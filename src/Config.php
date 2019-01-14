67471f0bc16be571763219f61ed6f2cfaffd9808
Martynas <jwebas@gmail.com>
2019-01-14 11:20:07 +0000

    Updated Config

--- src/Config.php
+++ src/Config.php
@@ -5,9 +5,127 @@ declare(strict_types=1);
 namespace Jwb;
 
 
-use Illuminate\Config\Repository;
+use ArrayAccess;
+use Jwb\Utils\Arr;
 
-class Config extends Repository
+class Config implements ArrayAccess
 {
+    /**
+     * All items
+     *
+     * @var array
+     */
+    protected $items = [];
 
+    /**
+     * Config constructor.
+     *
+     * @param array $items
+     */
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Get the specified configuration value.
+     *
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    public function get(string $key, $default = null)
+    {
+        return Arr::get($this->items, $key, $default);
+    }
+
+    /**
+     * Determine if the given configuration value exists.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function has(string $key): bool
+    {
+        return Arr::has($this->items, $key);
+    }
+
+    /**
+     * Set a given configuration value.
+     *
+     * @param array|string $key
+     * @param mixed        $value
+     */
+    public function set($key, $value = null): void
+    {
+        $keys = is_array($key) ? $key : [$key => $value];
+
+        foreach ($keys as $arrayKey => $arrayValue) {
+            Arr::set($this->items, $arrayKey, $arrayValue);
+        }
+    }
+
+    /**
+     * Get all of the configuration items for the application.
+     *
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->items;
+    }
+
+    /*
+     * ------------------------
+     * ArrayAccess methods
+     * ------------------------
+     *
+     * */
+
+    /**
+     * Determine if the given configuration option exists.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function offsetExists($key): bool
+    {
+        return $this->has($key);
+    }
+
+    /**
+     * Get a configuration option.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Set a configuration option.
+     *
+     * @param string $key
+     * @param mixed  $value
+     */
+    public function offsetSet($key, $value): void
+    {
+        $this->set($key, $value);
+    }
+
+    /**
+     * Unset a configuration option.
+     *
+     * @param string $key
+     */
+    public function offsetUnset($key): void
+    {
+        $this->set($key);
+    }
 }
